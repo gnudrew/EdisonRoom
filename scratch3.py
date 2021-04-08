44c9fc4bae5e2b76fac0d3c2fd2810c31527f543
gnudrew <gnudrew@gmail.com>
2021-04-07 19:57:36 -0500

    .

--- scratch3.py
+++ scratch3.py
@@ -1,6 +1,6 @@
 # Test how class structure works for import from subfolders.
 # Result -> We can treat folder hierarchies just like class object hierarchies.
-# e.g. path ""./fox/fox1/", which contains the file, "foxy.py", which contains a function fox(), is imported below.
+# e.g. path ""./fox/fox1/", which contains the file, "foxy.py", which contains a function "fox()", is imported below.
 
 from fox.fox1.foxy import fox
 
